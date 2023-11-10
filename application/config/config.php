c6e32de7787bd5985af11d73ba06a5f1b378b528
M Reza Aditiya <mrezadit@gmail.com>
2023-11-11 06:08:03 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pengiriman/';
+$config['base_url'] = 'http://localhost/cargosystem/';
 
 /*
 |--------------------------------------------------------------------------
